eba915efd4545830bfee71468d527036d69e01fa
takuya875 <57318943+takuya875@users.noreply.github.com>
2020-02-17 17:48:53 +0900

    commit
    修正

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,19 @@
+class TweetsController < ApplicationController
+
+  def index
+    @tweets = Tweet.all.order("created_at DESC").page(params[:page]).per(5)
+  end
+
+  def new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.permit(:date,:name, :image, :text)
+  end
+
+end
